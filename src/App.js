57656aa9829f69b64a59ca85f6cc8ed24b941b19
Your Name <you@example.com>
2022-05-30 04:34:02 +0700

    Base CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Button from './components/Button';
-import './App.css';
-
 function App() {
     return (
         <div className="App">
-            <Button />
+            <h1>React App</h1>
         </div>
     );
 }
--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-    alert('Say Hello');
-};
-
-export default myAlert;
